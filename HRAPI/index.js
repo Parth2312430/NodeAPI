a9ab1b9ef2b2c343c68fd1c373b53389d75fb044
Parth2312430 <bscs2312430@szabist.pk>
2025-05-02 05:30:42 +0000

    GET METHOD

---
+++ HRAPI/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require ('cors');
+const pool = require('./db');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/',async(req,res)=>{
+    try{
+        res.json('WELCOME TO HR API');
+    }catch(err){
+        res.status(500).json({Error:err.message});
+    }
+    
+});
+
+app.get('/country',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from countries');
+        res.json(result.rows);
+    }catch(err)
+    {
+        res.status(500).json({Error:err.message});
+    }
+})
+
+app.get('/departments',async(req,res)=>{
+    try{
+        const result = await pool.query('select ');
+        res.json(result.rows);
+    }catch(err)
+    {
+        res.status(500).json({Error:err.message});
+    }
+})
+
+app.get('/region',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from regions');
+        res.json(result.rows);
+    }catch(err)
+    {
+        res.status(500).json({Error:err.message});
+    }
+})
+
+app.get('/employees',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from employees');
+        res.json(result.rows);
+    }catch(err)
+    {
+        res.status(500).json({Error:err.message});
+    }
+})
+
+const PORT = process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`Connected Succefully....on PORT ${PORT}`);
+});
